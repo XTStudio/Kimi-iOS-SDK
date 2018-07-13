3780cfe692eeb7ccc95f02a8777c9b4f2ab39afb
PonyCui <cuis@vip.qq.com>
2018-07-13 10:16:26 +0800

    use HTTP for Unit Test.

--- Kimi-iOS-SDKTests/Foundation/URLSessionTests.m
+++ Kimi-iOS-SDKTests/Foundation/URLSessionTests.m
@@ -20,7 +20,7 @@
     JSContext *context = [AppDelegate unitTestContext];
     KimiTestExpectation *sessionGetTest = [[KimiTestExpectation alloc] init];
     [AppDelegate setUnitTestObject:sessionGetTest forKey:@"sessionGetTest"];
-    [context evaluateScript:@"URLSession.shared.dataTask('https://httpbin.org/get', function(data, response, error){ if (JSON.parse(data.utf8String()).url === 'https://httpbin.org/get'){ sessionGetTest.fulfill() } }).resume()"];
+    [context evaluateScript:@"URLSession.shared.dataTask('http://httpbin.org/get', function(data, response, error){ if (JSON.parse(data.utf8String()).url === 'http://httpbin.org/get'){ sessionGetTest.fulfill() } }).resume()"];
     [self waitForExpectations:@[sessionGetTest] timeout:10];
 }
 
@@ -28,8 +28,8 @@
     JSContext *context = [AppDelegate unitTestContext];
     KimiTestExpectation *sessionGetTest = [[KimiTestExpectation alloc] init];
     [AppDelegate setUnitTestObject:sessionGetTest forKey:@"sessionGetTest"];
-    [context evaluateScript:@"var req = new MutableURLRequest('https://httpbin.org/get');"];
-    [context evaluateScript:@"URLSession.shared.dataTask(req, function(data, response, error){ if (JSON.parse(data.utf8String()).url === 'https://httpbin.org/get'){ sessionGetTest.fulfill() } }).resume()"];
+    [context evaluateScript:@"var req = new MutableURLRequest('http://httpbin.org/get');"];
+    [context evaluateScript:@"URLSession.shared.dataTask(req, function(data, response, error){ if (JSON.parse(data.utf8String()).url === 'http://httpbin.org/get'){ sessionGetTest.fulfill() } }).resume()"];
     [self waitForExpectations:@[sessionGetTest] timeout:10];
 }
 
@@ -37,7 +37,7 @@
     JSContext *context = [AppDelegate unitTestContext];
     KimiTestExpectation *sessionPostTest = [[KimiTestExpectation alloc] init];
     [AppDelegate setUnitTestObject:sessionPostTest forKey:@"sessionPostTest"];
-    [context evaluateScript:@"var req = new MutableURLRequest(URL.URLWithString('https://httpbin.org/post')); req.HTTPMethod = 'POST'; req.setValueForHTTPHeaderField('application/x-www-form-urlencoded', 'Content-Type'); req.HTTPBody = new Data({utf8String: 'username=PonyCui'})"];
+    [context evaluateScript:@"var req = new MutableURLRequest(URL.URLWithString('http://httpbin.org/post')); req.HTTPMethod = 'POST'; req.setValueForHTTPHeaderField('application/x-www-form-urlencoded', 'Content-Type'); req.HTTPBody = new Data({utf8String: 'username=PonyCui'})"];
     [context evaluateScript:@"URLSession.shared.dataTask(req, function(data, response, error){ if (JSON.parse(data.utf8String()).form.username === 'PonyCui'){ sessionPostTest.fulfill() } }).resume()"];
     [self waitForExpectations:@[sessionPostTest] timeout:10];
 }
@@ -46,7 +46,7 @@
     JSContext *context = [AppDelegate unitTestContext];
     KimiTestExpectation *sessionStatusCodeTest = [[KimiTestExpectation alloc] init];
     [AppDelegate setUnitTestObject:sessionStatusCodeTest forKey:@"sessionStatusCodeTest"];
-    [context evaluateScript:@"URLSession.shared.dataTask('https://httpbin.org/status/233', function(data, response, error){ if (response.statusCode === 233){ sessionStatusCodeTest.fulfill() } }).resume()"];
+    [context evaluateScript:@"URLSession.shared.dataTask('http://httpbin.org/status/233', function(data, response, error){ if (response.statusCode === 233){ sessionStatusCodeTest.fulfill() } }).resume()"];
     [self waitForExpectations:@[sessionStatusCodeTest] timeout:10];
 }
 
