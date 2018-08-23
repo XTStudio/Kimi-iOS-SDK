5dcea0f4b2064bfc833dfb60b67bc6e064038888
PonyCui <cuis@vip.qq.com>
2018-08-23 15:21:04 +0800

    Feature: add hostVersion field to KMCore.

---
+++ Kimi-iOS-SDKTests/KMCore/KMCoreTestCase.m
@@ -0,0 +1,33 @@
+//
+//  KMCoreGTestCase.m
+//  Kimi-iOS-SDKTests
+//
+//  Created by 崔 明辉 on 2018/8/23.
+//  Copyright © 2018年 XT Studio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "AppDelegate.h"
+#import "KimiTestExpectation.h"
+
+@interface KMCoreTestCase : XCTestCase
+
+@end
+
+@implementation KMCoreTestCase
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)testVersion {
+    JSContext *context = [AppDelegate unitTestContext];
+    XCTAssertTrue([[context evaluateScript:@"KMCore.version"].toString isEqualToString:@"0.1.0"]);
+}
+
+- (void)testHostVersion {
+    JSContext *context = [AppDelegate unitTestContext];
+    XCTAssertTrue([[context evaluateScript:@"KMCore.hostVersion"].toString isEqualToString:@"1.0"]);
+}
+
+@end
