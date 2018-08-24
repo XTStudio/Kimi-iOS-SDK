a543f00e5229ae63772ceebddbcc559a60aba6df
PonyCui <cuis@vip.qq.com>
2018-08-24 11:51:52 +0800

    Bug-fixed: The UITabBarController viewDidLoad method invoke too fast.

--- Source/UIKit/UITabBarController+Kimi.m
+++ Source/UIKit/UITabBarController+Kimi.m
@@ -9,6 +9,26 @@
 #import "UITabBarController+Kimi.h"
 #import <Endo/EDOExporter.h>
 
+@interface KIMITabBarController: UITabBarController
+
+@property (nonatomic, assign) BOOL kimi_viewDidLoadCalled;
+
+@end
+
+@implementation KIMITabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    if (!self.kimi_viewDidLoadCalled) {
+        self.kimi_viewDidLoadCalled = YES;
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self viewDidLoad];
+        });
+    }
+}
+
+@end
+
 @implementation UITabBarController (Kimi)
 
 + (void)load {
@@ -17,6 +37,9 @@
     EDO_EXPORT_PROPERTY(@"selectedViewController");
     EDO_EXPORT_METHOD_ALIAS(setViewControllers:animated:, @"setViewControllers");
     EDO_EXPORT_READONLY_PROPERTY(@"tabBar");
+    EDO_EXPORT_INITIALIZER({
+        return [[KIMITabBarController alloc] init];
+    })
 }
 
 @end
