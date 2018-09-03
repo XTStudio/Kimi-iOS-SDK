d7f81c5e0d44eb2123d227f5b31e2005e54e4d97
PonyCui <cuis@vip.qq.com>
2018-09-03 15:21:54 +0800

    Feature: let UIScrollView scroll to top after beginRefreshing method call.

--- Source/UIKit/UIRefreshControl+Kimi.m
+++ Source/UIKit/UIRefreshControl+Kimi.m
@@ -16,13 +16,25 @@
     EDO_EXPORT_PROPERTY(@"enabled");
     EDO_EXPORT_READONLY_PROPERTY(@"refreshing");
     EDO_EXPORT_PROPERTY(@"tintColor");
-    EDO_EXPORT_METHOD(beginRefreshing);
+    EDO_EXPORT_METHOD(edo_beginRefreshing);
     EDO_EXPORT_METHOD(endRefreshing);
     [[EDOExporter sharedExporter] exportInitializer:[self class] initializer:^id(NSArray *arguments) {
         UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
         [refreshControl addTarget:refreshControl action:@selector(edo_handleValueChanged) forControlEvents:UIControlEventValueChanged];
         return refreshControl;
     }];
+}
+
+- (void)edo_beginRefreshing {
+    if ([self.superview isKindOfClass:[UIScrollView class]]) {
+        UIScrollView *scrollView = (UIScrollView *)self.superview;
+        [self beginRefreshing];
+        [scrollView setContentOffset:CGPointMake(0, -54.0)
+                            animated:YES];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.75 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self edo_emitWithEventName:@"refresh" arguments:@[self]];
+        });
+    }
 }
 
 - (void)edo_handleValueChanged {
