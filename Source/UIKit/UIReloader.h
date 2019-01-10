69aede4faedfa65aeefa79a160a738a0029faab4
PonyCui <cuis@vip.qq.com>
2019-01-10 17:36:59 +0800

    feat: Add hot reload support.

---
+++ Source/UIKit/UIReloader.h
@@ -0,0 +1,17 @@
+//
+//  UIReloader.h
+//  Kimi-iOS-SDK
+//
+//  Created by PonyCui on 2019/1/10.
+//  Copyright © 2019年 XT Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIReloader : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
